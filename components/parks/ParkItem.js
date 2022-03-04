ae707626d4064b62d4cf14474a11b8cbc81d5326
Mariya Lazarova <s1227664@sms.ed.ac.uk>
2022-03-04 16:50:14 +0000

    fix(park-item): fix linting error

--- components/parks/ParkItem.js
+++ components/parks/ParkItem.js
@@ -1,11 +1,11 @@
+import Image from 'next/image'
 import classes from './ParkItem.module.css';
 
 const ParkItem = (props) =>{
   return (
     <li className={classes.item}>
       <div className={classes.image}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={props.image} alt={props.title}/>
+        <Image src={props.image} alt={props.title}/>
       </div>
       <div className={classes.description}>
         <h3>{props.title}</h3>
