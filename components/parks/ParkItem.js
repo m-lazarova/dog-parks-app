3f4e93f89398a9e82557115b7c903196e36f5de6
Mariya Lazarova <s1227664@sms.ed.ac.uk>
2022-03-04 16:41:54 +0000

    style(park-item): add css to park item

--- components/parks/ParkItem.js
+++ components/parks/ParkItem.js
@@ -1,11 +1,13 @@
+import classes from './ParkItem.module.css';
+
 const ParkItem = (props) =>{
   return (
-    <li>
-      <div>
+    <li className={classes.item}>
+      <div className={classes.image}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={props.image} alt={props.title}/>
       </div>
-      <div>
+      <div className={classes.description}>
         <h3>{props.title}</h3>
         <address>{props.address}</address>
       </div>
